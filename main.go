1f9f5b98bf4da0793fd7df8d9f4f536cd38d53cf
Luviichann <anddiaoling777@163.com>
2024-07-13 18:13:15 +0800

    main

--- main.go
+++ main.go
@@ -11,13 +11,9 @@ import (
 func main() {
 	r := gin.Default()
 	cors := func(c *gin.Context) {
-		// 允许特定的域进行跨域请求
 		c.Writer.Header().Set("Access-Control-Allow-Origin", models.CORS)
-		// 允许特定的请求方法
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		// 允许特定的请求头
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		// 允许携带身份凭证（如Cookie）
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusOK)
@@ -31,11 +27,11 @@ func main() {
 
 	r.Static("/static/image", "./static/image")
 	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Hello World!") //http.StatusOK就等于200。
+		ctx.String(http.StatusOK, "Hello World!")
 	})
 	routers.IndexRouter(r)
 	routers.MsgBoardRouter(r)
 	routers.SubscribeRouter(r)
 	routers.AdminRouter(r)
-	r.Run(":8820") //默认端口是8000。括号里可以指定端口号，接收字符串类型，比如":4000"。注意":"不能省略！
+	r.Run(":8820")
 }
