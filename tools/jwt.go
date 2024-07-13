8b906e21ce31e39facc69700247b00ed22ccb92d
Luviichann <anddiaoling777@163.com>
2024-07-13 17:20:33 +0800

    入口，工具

---
+++ tools/jwt.go
@@ -0,0 +1,34 @@
+package tools
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("a_secret_crect")
+
+type Claims struct {
+	Key string
+	jwt.RegisteredClaims
+}
+
+func ReleaseToken(key string) (string, error) {
+	expirationTime := time.Now().Add(7 * 86400 * time.Second)
+	claims := Claims{
+		Key: key,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			// 这个Issuer可能要放进配置文件中。
+			Issuer:  "faruzan.cn",
+			Subject: "token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
